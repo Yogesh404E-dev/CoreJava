2a4d3e364181d0e6b35fe587dc71f5b38240e27f
Yogesh <yogesh@healerite.com>
2021-06-20 09:38:50 -0400

    Input scanner class

---
+++ InputFunctions.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class InputFunctions{
+    public static void main(String[] args) 
+ {
+     String name;int a;float b;double c;
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter a  String =:");
+     name = sc.next();
+     System.out.println("Enter a  Integer =:");
+     a = sc.nextInt();
+     System.out.println("Enter a  Float =:");
+     b = sc.nextFloat();
+     System.out.println("Enter a  Double =:");
+     c = sc.nextDouble();
+    
+
+ }
+
+}
