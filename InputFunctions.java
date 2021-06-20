90a42db67058433a4c5b44afac7606c184d7fdf3
Yogesh <yogesh@healerite.com>
2021-06-20 09:44:55 -0400

    Input functions with Scanner class

--- InputFunctions.java
+++ InputFunctions.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
+//Import scanner util package with scanner class
 class InputFunctions{
     public static void main(String[] args) 
  {
+     //creating variables for string int float and double data-type
      String name;int a;float b;double c;
      Scanner sc = new Scanner(System.in);
      System.out.println("Enter a  String =:");
-     name = sc.next();
+     name = sc.next();//For String data type input from keyboard
      System.out.println("Enter a  Integer =:");
-     a = sc.nextInt();
+     a = sc.nextInt();//For Integer data type input from keyboard
      System.out.println("Enter a  Float =:");
-     b = sc.nextFloat();
+     b = sc.nextFloat();//For Float data type input from keyboard
      System.out.println("Enter a  Double =:");
-     c = sc.nextDouble();
+     c = sc.nextDouble();//For Double data type input from keyboard
     
 
  }
