7785423f73a50a8866d3cd0f68e129080be6a113
Yogesh <yogesh@healerite.com>
2021-06-20 09:54:25 -0400

    removed variable declare and than use

--- InputFunctions.java
+++ InputFunctions.java
@@ -3,17 +3,15 @@ import java.util.Scanner;
 class InputFunctions{
     public static void main(String[] args) 
  {
-     //creating variables for string int float and double data-type
-     String name;int a;float b;double c;
      Scanner sc = new Scanner(System.in);
      System.out.println("Enter a  String =:");
-     name = sc.next();//For String data type input from keyboard
+     String name = sc.next();//For String data type input from keyboard
      System.out.println("Enter a  Integer =:");
-     a = sc.nextInt();//For Integer data type input from keyboard
+     int a = sc.nextInt();//For Integer data type input from keyboard
      System.out.println("Enter a  Float =:");
-     b = sc.nextFloat();//For Float data type input from keyboard
+     float b = sc.nextFloat();//For Float data type input from keyboard
      System.out.println("Enter a  Double =:");
-     c = sc.nextDouble();//For Double data type input from keyboard
+     double c = sc.nextDouble();//For Double data type input from keyboard
     
 
  }
