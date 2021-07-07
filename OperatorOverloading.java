a1328d4718dac2797bebb484bcd9d568ad7a4772
Yogesh <yogesh@healerite.com>
2021-07-07 11:22:39 -0400

    operator overloading

---
+++ OperatorOverloading.java
@@ -0,0 +1,10 @@
+class OperatorOverloading{
+    int a = 5; int b= 7;
+    public static void main(String arg[]){
+        OperatorOverloading obj = new OperatorOverloading();
+        System.out.println("a + b = "+obj.a+obj.b);//for concat
+        System.out.println("a + b = " +(obj.a+obj.b));//For addition
+    }
+}
+
+//o/p -> https://i.imgur.com/uPiRlnr.png
