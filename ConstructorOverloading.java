14607a7d33902a4f89faa33e5679db251a4950c7
Yogesh <yogesh@healerite.com>
2021-07-07 01:08:00 -0400

    constructor overloading example

--- ConstructorOverloading.java
+++ ConstructorOverloading.java
@@ -1,15 +1,19 @@
 class A{
+    //default constructor 
     A(){
         System.out.println("I m constructor 1");
     }
+    //parameterized constructor
     A(int a){
         System.out.println("I m constorer 2 and a="+a);
     }
 }
 
 class ConstructorOverloading{
+
     public static void main(String agrs[]){
-        A obj1 = new A();
-        A obj2 = new A(10);
+
+        A obj1 = new A();//call default construcotr
+        A obj2 = new A(10); //parameterized 
     }
 }
